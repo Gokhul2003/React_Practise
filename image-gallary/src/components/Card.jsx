32dc7323b0d6324fae6ee2cb1ff5893b37f06685
= <=>
2026-01-12 12:43:42 +0530

    image gallary completed

---
+++ image-gallary/src/components/Card.jsx
@@ -0,0 +1,15 @@
+const Card = (props) => {
+  return (
+    <div>
+        <a href={props.elem.url} target='_blank' rel='noreferrer'>
+            <div className='h-40 w-40 overflow-hidden rounded-xl'>
+                <img className='h-full w-full object-cover' src={props.elem.download_url} alt={props.elem.author} />
+            </div>
+            <h2 classNa='font-bold text-lg'>{props.elem.author}</h2>
+        </a>
+    
+    </div>
+  )
+}
+
+export default Card
